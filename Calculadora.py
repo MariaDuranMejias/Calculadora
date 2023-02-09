36c917cdbd7c943f46614ff1de2913bb24e0a271
MariaDuranMejias <mjduran03@gmail.com>
2023-02-08 19:14:41 -0600

    Third commit

--- Calculadora.py
+++ Calculadora.py
@@ -4,22 +4,50 @@ import Operaciones as op
 print("Estas son las operaciones disponibles: \n1 = Suma: entre n números \n2 = Resta: entre 2 números \n3 = Multiplicación: entre n números \n4 = División: entre 2 número \n5 = Factorial: de 1 número \n6 = Potencia: 1 número elevado al otro")
 a = int(input("Digite el digito de la operacion que desea realizar: \n"))
 
-if a==1:
-    print("Resultado: " + str(op.sumatoria()))
+resultado = str("")
+file = open("resultados.txt", "a")
+#file.write("Se realizo la siguiente operacion: " +  strb + "/n")
+#file.write("El resultado es" + resultado + "/n")
 
+if a==1:
+    b = str("Suma")
+    resultado = str(op.sumatoria())
+    file.write("Se realizo la siguiente operacion: " +  str(b) + "\n")
+    file.write("El resultado es: " + resultado + "\n")
+    print("Resultado: " + resultado)
 elif a==2:
-    print("Resultado: " + str(op.resta()))
+    b = str("Resta")
+    resultado = str(op.resta())
+    file.write("Se realizo la siguiente operacion: " +  str(b) + "\n")
+    file.write("El resultado es: " + resultado + "\n")
+    print("Resultado: " + resultado)
 
 elif a==3:
-    print("Resultado: " + str(op.multiplicacion()))
+    b = str("Multiplicacion")
+    resultado = str(op.multiplicacion())
+    file.write("Se realizo la siguiente operacion: " +  str(b) + "\n")
+    file.write("El resultado es: " + resultado + "\n")
+    print("Resultado: " + resultado)
 
 elif a==4:
-    print("Resultado: " + str(op.division()))
+    b = str("Division")
+    resultado = str(op.division())
+    file.write("Se realizo la siguiente operacion: " +  str(b) + "\n")
+    file.write("El resultado es: " + resultado + "\n")
+    print("Resultado: " + resultado)
 
 elif a==5:
-    print("Resultado: " + str(op.factorial()))
+    b = str("Factorial")
+    resultado = str(op.factorial())
+    file.write("Se realizo la siguiente operacion: " +  str(b) + "\n")
+    file.write("El resultado es: " + resultado + "\n")
+    print("Resultado: " + resultado)
 
 elif a==6:
-    print("Resultado: " + str(op.potencia()))
+    b = str("Potencia")
+    resultado = str(op.potencia())
+    file.write("Se realizo la siguiente operacion: " +  str(b) + "\n")
+    file.write("El resultado es: " + resultado + "\n")
+    print("Resultado: " + resultado)
 else:
     print("Error: Operacion invalida")
